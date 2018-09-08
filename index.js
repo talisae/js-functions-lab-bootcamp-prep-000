ac7e27fc9da62349cb8c146158e55174ee07cee2
Talisa Erazo <talisaerazo@gmail.com>
2018-09-08 17:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday){
+function holidayCountdown(holiday, number){
   return `It's ${days} days until ${holiday}!`
 }
